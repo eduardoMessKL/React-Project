dd156af62bdaf1ecee6ba0e9b9d954920d92c9ae
eduardoMessKL <eduardomk408@gmail.com>
2024-08-13 18:59:47 -0300

    Alteração na main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Facebook from './components/Facebook.jsx'
+import Instagram  from './components/Instagram.jsx' 
+
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Facebook />
+    <Instagram />
   </StrictMode>,
 )
