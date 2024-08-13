709fa7eb08e1ae2d104ec98ffc09fda489096420
eduardoMessKL <eduardomk408@gmail.com>
2024-08-13 20:19:25 -0300

    Width

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <header className='bg-red-400'>
+      <header className='bg-red-400 w-400'>
         <h1>Bem vindo ao meu site!</h1>
       </header>
     </div>
