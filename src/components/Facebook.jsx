faa189c1549a19199722e6371a95baca7afc21b4
eduardoMessKL <eduardomk408@gmail.com>
2024-08-13 18:27:34 -0300

    Criação do Componente Instagram

--- src/components/Facebook.jsx
+++ src/components/Facebook.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Facebook = () => {
   return (
     <div>
-      
+      <h1>Facebook!</h1>
     </div>
   )
 }
---
+++ src/components/Instagram.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Instagram = () => {
+  return (
+    <div>
+      <h1>Instagram</h1>
+    </div>
+  )
+}
+
+export default Instagram
